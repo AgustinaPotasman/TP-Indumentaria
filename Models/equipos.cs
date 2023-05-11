55dd3a3e934e46859aa21bd420c12767b966ed63
MATIAS IVAN SZABO <47805194@ort>
2023-05-11 15:54:56 -0300

    Create .NET MVC Project

---
+++ Models/equipos.cs
@@ -0,0 +1,8 @@
+static class Equipos
+{
+    public List<string> ListaEquipos {get; private set;}
+    public List<string> ListaMedias {get; private set;}
+    public List<string> ListaPantalones {get; private set;}
+    public List<string> ListaRemeras {get; private set;}
+    public Dictionary<string, indumentaria> EquiposIndumentaria {get; private set;}
+}
