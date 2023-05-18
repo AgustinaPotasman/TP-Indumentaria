6571bffa50f3a1555c5a4fb99ef3fb56d2a74f01
AGUSTINA POTASMAN <47572491@ort>
2023-05-18 15:16:00 -0300

    Create .NET MVC Project

--- Models/equipos.cs
+++ Models/equipos.cs
@@ -8,10 +8,10 @@ static class Equipos
 
     static Equipos()
     {
-        ListaEquipos = new List<string>{"Equipo1", "Equipo2", "Equipo3", "Equipo4", "Equipo5", "Equipo6", "Equipo7", "Equipo8", "Equipo9", "Equipo10"};
-        ListaMedias = new List<string>{"media1.jpg", "media2.jpg", "media3.jpg", "media4.jpg", "media5.jpg", "media6.jpg", "media7.jpg", "media8.jpg", "media9.jpg", "media10.jpg"};
-        ListaPantalones = new List<string>{"pantalon1.jpg", "pantalon2.jpg", "pantalon3.jpg", "pantalon4.jpg", "pantalon5.jpg", "pantalon6.jpg", "pantalon7.jpg", "pantalon8.jpg", "pantalon9.jpg", "pantalon10.jpg"};
-        ListaRemeras = new List<string>{"remera1.jpg", "remera2.jpg", "remera3.jpg", "remera4.jpg", "remera5.jpg", "remera6.jpg", "remera7.jpg", "remera8.jpg", "remera9.jpg", "remera10.jpg"};
+        ListaEquipos = new List<string>{"Equipo1", "Equipo2", "Equipo3", "Equipo4", "Equipo5"};
+        ListaMedias = new List<string>{"media1.jpg", "media2.jpg", "media3.jpg", "media4.jpg", "media5.jpg"};
+        ListaPantalones = new List<string>{"pantalon1.jpg", "pantalon2.jpg", "pantalon3.jpg", "pantalon4.jpg", "pantalon5.jpg"};
+        ListaRemeras = new List<string>{"remera1.jpg", "remera2.jpg", "remera3.jpg", "remera4.jpg", "remera5.jpg"};
         EquiposIndumentaria = new Dictionary<string, Indumentaria>();
     }
 
